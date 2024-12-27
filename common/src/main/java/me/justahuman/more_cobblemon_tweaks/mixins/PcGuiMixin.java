512d83c08b54a18dfc8331b39949c866f1d60d0b
JustAHuman-xD <65748158+JustAHuman-xD@users.noreply.github.com>
2024-12-26 21:30:23 -0600

    fix closing when typing e in search or rename

--- common/src/main/java/me/justahuman/more_cobblemon_tweaks/mixins/PcGuiMixin.java
+++ common/src/main/java/me/justahuman/more_cobblemon_tweaks/mixins/PcGuiMixin.java
@@ -1,6 +1,7 @@
 package me.justahuman.more_cobblemon_tweaks.mixins;
 
 import com.cobblemon.mod.common.client.gui.pc.PCGUI;
+import com.cobblemon.mod.common.util.MiscUtilsKt;
 import me.justahuman.more_cobblemon_tweaks.config.ModConfig;
 import me.justahuman.more_cobblemon_tweaks.features.pc.IvWidget;
 import me.justahuman.more_cobblemon_tweaks.features.pc.box_name.CancelButton;
@@ -12,6 +13,7 @@ import me.justahuman.more_cobblemon_tweaks.features.pc.search.SearchWidget;
 import me.justahuman.more_cobblemon_tweaks.features.pc.wallpaper.WallpaperButton;
 import me.justahuman.more_cobblemon_tweaks.features.pc.wallpaper.WallpaperWidget;
 import me.justahuman.more_cobblemon_tweaks.utils.Utils;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.components.Renderable;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.CommonComponents;
@@ -24,6 +26,7 @@ import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.HashSet;
@@ -33,6 +36,9 @@ import java.util.Set;
 public abstract class PcGuiMixin extends Screen {
     @Shadow(remap = false) @Final public static int BASE_WIDTH;
     @Shadow(remap = false) @Final public static int BASE_HEIGHT;
+
+    @Unique private RenameWidget moreCobblemonTweaks$renameWidget;
+    @Unique private SearchWidget moreCobblemonTweaks$searchWidget;
 
     protected PcGuiMixin(Component title) {
         super(title);
@@ -61,13 +67,13 @@ public abstract class PcGuiMixin extends Screen {
         if (ModConfig.isEnabled("custom_pc_box_names")) {
             siblings.add(this.addRenderableWidget(new CancelButton(x + 243, y - 13, siblings)));
             siblings.add(this.addRenderableWidget(new ConfirmButton(x + 222, y - 13, siblings)));
-            siblings.add(this.addRenderableWidget(new RenameWidget(x + 106, y - 13)));
+            siblings.add(this.addRenderableWidget(moreCobblemonTweaks$renameWidget = new RenameWidget(x + 106, y - 13)));
             siblings.add(this.addRenderableWidget(new RenameButton(x + (wallpapers ? 220 : 241), y - 13, siblings)));
         }
 
         if (ModConfig.isEnabled("pc_search")) {
             siblings.add(this.addRenderableWidget(new SearchButton(x + 82, y - 13, siblings)));
-            siblings.add(this.addRenderableWidget(new SearchWidget(x + 104, y - 13)));
+            siblings.add(this.addRenderableWidget(moreCobblemonTweaks$searchWidget = new SearchWidget(x + 104, y - 13)));
         }
     }
 
@@ -80,6 +86,13 @@ public abstract class PcGuiMixin extends Screen {
             }
         }
         return text;
+    }
+
+    @Redirect(method = "keyPressed", at = @At(value = "INVOKE", target = "Lcom/cobblemon/mod/common/util/MiscUtilsKt;isInventoryKeyPressed(Lnet/minecraft/client/gui/screens/Screen;Lnet/minecraft/client/Minecraft;II)Z"))
+    public boolean preventClosing(Screen $this$isInventoryKeyPressed, Minecraft client, int keyCode, int scanCode) {
+        boolean renameSelected = moreCobblemonTweaks$renameWidget != null && moreCobblemonTweaks$renameWidget.isFocused();
+        boolean searchSelected = moreCobblemonTweaks$searchWidget != null && moreCobblemonTweaks$searchWidget.isFocused();
+        return MiscUtilsKt.isInventoryKeyPressed($this$isInventoryKeyPressed, client, keyCode, scanCode) && !renameSelected && !searchSelected;
     }
 
     @Unique
