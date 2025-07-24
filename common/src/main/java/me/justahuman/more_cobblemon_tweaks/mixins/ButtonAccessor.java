bde91f9dc24d01fb09ba60cb98637a1d7104e24c
JustAHuman-xD <imjustahuman.xd@gmail.com>
2025-07-23 20:23:13 -0500

    1.1.3
    fixes #27

--- common/src/main/java/me/justahuman/more_cobblemon_tweaks/mixins/ButtonAccessor.java
+++ common/src/main/java/me/justahuman/more_cobblemon_tweaks/mixins/ButtonAccessor.java
@@ -3,10 +3,11 @@ package me.justahuman.more_cobblemon_tweaks.mixins;
 import net.minecraft.client.gui.components.Button;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 @Mixin(Button.class)
 public interface ButtonAccessor {
     @Accessor @Final Button.OnPress getOnPress();
-    @Accessor @Final void setOnPress(Button.OnPress onPress);
+    @Accessor @Final @Mutable void setOnPress(Button.OnPress onPress);
 }
